a01548f955fcfee8bb94f9d2c6ead56dc22c9826
Amer alghamdi <103968351+Akom07@users.noreply.github.com>
2024-05-22 05:47:40 +0300

    done

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
 let musicians = 0
 if (musicians <= 0){
 console.log("not a group");
-} else if (musicians = 1)
+} else if (musicians == 1)
     {
     console.log("solo");
 
-} else if (musicians = 2){
+} else if (musicians == 2){
     console.log("duet");
 
-} else if (musicians=3)
+} else if (musicians==3)
     {
     console.log("trio");
 
-} else if (musicians=4){
+} else if (musicians==4){
     console.log("quartet");
 
 } else if (musicians>=5){
     console.log("this is a large group");
 
-}else{
-    console.log("non");
 }
 //-------------------------------------
 let room = 'ballroom'
